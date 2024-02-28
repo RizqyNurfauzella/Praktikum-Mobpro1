80dec71346332d32258db5f7d608ac753bcb7a73
Rizqy Nurfauzella <fauzella171@gmail.com>
2024-02-28 13:47:07 +0700

    Membuat Tampilan Counter

--- app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Counter()
                 }
             }
         }
@@ -56,14 +63,26 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
     }
 }
 
-
 @Composable
-fun Greeting(name: String) {
+fun Counter() {
     MainScreen {modifier ->
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "0",
+                style = MaterialTheme.typography.displayLarge
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(id = R.string.count))
+            }
+        }
     }
 }
 
@@ -72,6 +91,6 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     Mobpro1Theme {
-        Greeting("Android")
+        Counter()
     }
 }
