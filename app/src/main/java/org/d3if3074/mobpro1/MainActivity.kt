e358d0a3b7e0909dec782762cde02d2c703b215c
Rizqy Nurfauzella <fauzella171@gmail.com>
2024-03-06 12:30:33 +0700

    Menambahkan data hewan

--- app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
@@ -17,9 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3074.mobpro1.model.Hewan
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
+
+    private val data = getData()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,6 +36,16 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Ayam", R.drawable.ayam),
+            Hewan("Bebek", R.drawable.bebek),
+            Hewan("Domba", R.drawable.domba),
+            Hewan("Kambing", R.drawable.kambing),
+            Hewan("Sapi", R.drawable.sapi),
+        )
     }
 }
 
---
+++ app/src/main/java/org/d3if3074/mobpro1/model/Hewan.kt
@@ -0,0 +1,8 @@
+package org.d3if3074.mobpro1.model
+
+import androidx.annotation.DrawableRes
+
+data class Hewan(
+    val nama: String,
+    @DrawableRes val imageResId: Int
+)
