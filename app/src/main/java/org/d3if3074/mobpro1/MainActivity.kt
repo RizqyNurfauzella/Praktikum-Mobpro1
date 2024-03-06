4307ca415f908a174ba99ef72927e46357858f0a
Rizqy Nurfauzella <fauzella171@gmail.com>
2024-03-06 13:21:23 +0700

    Membuat tombol lanjut

--- app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3074/mobpro1/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,6 +21,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -31,6 +37,7 @@ import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 class MainActivity : ComponentActivity() {
 
     private val data = getData()
+    private var index by mutableIntStateOf(0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,7 +47,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GaleriHewan(hewan = data[0])
+                    GaleriHewan(hewan = data[index]) {
+                        index++
+                    }
                 }
             }
         }
@@ -78,12 +87,10 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
 }
 
 @Composable
-fun GaleriHewan(hewan: Hewan) {
+fun GaleriHewan(hewan: Hewan, onClick: () -> Unit = {}) {
     MainScreen {modifier ->
         Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(16.dp),
+            modifier = modifier.fillMaxSize().padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -98,6 +105,13 @@ fun GaleriHewan(hewan: Hewan) {
                 style = MaterialTheme.typography.headlineLarge,
                 modifier = Modifier.padding(top = 16.dp)
             )
+            Button(
+                onClick = { onClick() },
+                modifier = Modifier.fillMaxWidth(0.5f).padding(top = 24.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(id = R.string.lanjut))
+            }
         }
     }
 }
