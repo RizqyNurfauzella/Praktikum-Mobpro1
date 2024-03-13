9546cedc2cddafb77caafb94ffe2a74210eeead9
RizqyNurfauzella <fauzella171@gmail.com>
2024-03-13 13:54:04 +0700

    Membuat perhitungan BMI

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,6 +26,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -39,6 +42,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if3074.mobpro1.R
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
+import kotlin.math.pow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -70,6 +74,9 @@ fun ScreenContent(modifier: Modifier) {
         stringResource(id = R.string.wanita)
     )
     var gender by remember { mutableStateOf(radioOptions[0]) }
+
+    var bmi by remember { mutableFloatStateOf(0f) }
+    var kategori by remember { mutableIntStateOf(0) }
 
     Column(
         modifier = modifier
@@ -129,11 +136,28 @@ fun ScreenContent(modifier: Modifier) {
             }
         }
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                bmi = hitungBmi(berat.toFloat(), tinggi.toFloat())
+                kategori = getKategori(bmi, gender == radioOptions[0])
+            },
             modifier = Modifier.padding(8.dp),
             contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
             Text(text = stringResource(id = R.string.hitung))
+        }
+        if (bmi !=0f) {
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = stringResource(id = R.string.bmi_x, bmi),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = stringResource(id = kategori).uppercase(),
+                style = MaterialTheme.typography.headlineLarge
+            )
         }
     }
 }
@@ -150,6 +174,26 @@ fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(start = 8.dp)
         )
+    }
+}
+
+private fun hitungBmi(berat: Float, tinggi: Float): Float {
+    return berat / (tinggi / 100).pow(2)
+}
+
+private fun getKategori(bmi: Float, isMale: Boolean): Int {
+    return if (isMale) {
+        when {
+            bmi < 20.5 -> R.string.kurus
+            bmi > 27.0 -> R.string.gemuk
+            else -> R.string.ideal
+        }
+    } else {
+        when {
+            bmi < 18.5 -> R.string.kurus
+            bmi > 25.0 -> R.string.gemuk
+            else -> R.string.ideal
+        }
     }
 }
 
