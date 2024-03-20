230171973da963038fb6a5d1616cec5545f5c731
RizqyNurfauzella <fauzella171@gmail.com>
2024-03-20 15:02:56 +0700

    Mengerjakan Challenge 05

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
@@ -1,5 +1,7 @@
 package org.d3if3074.mobpro1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,10 +14,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -26,16 +30,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3074.mobpro1.R
+import org.d3if3074.mobpro1.navigation.Screen
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
@@ -43,7 +51,7 @@ import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavHostController) {
     Scaffold (
       topBar = {
           TopAppBar(
@@ -53,7 +61,18 @@ fun MainScreen() {
               colors = TopAppBarDefaults.mediumTopAppBarColors(
                   containerColor = MaterialTheme.colorScheme.primaryContainer,
                   titleContentColor = MaterialTheme.colorScheme.primary
-              )
+              ),
+              actions = {
+                  IconButton(onClick = {
+                      navController.navigate(Screen.About.route)
+                  }) {
+                      Icon(
+                          imageVector = Icons.Outlined.Info,
+                          contentDescription = stringResource(id = R.string.tentang_aplikasi),
+                          tint = MaterialTheme.colorScheme.primary
+                      )
+                  }
+              }
           )
         }
     ) { padding ->
@@ -63,14 +82,14 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    var panjang by remember { mutableStateOf("") }
-    var panjangError by remember { mutableStateOf(false) }
+    var panjang by rememberSaveable { mutableStateOf("") }
+    var panjangError by rememberSaveable { mutableStateOf(false) }
 
-    var lebar by remember { mutableStateOf("") }
-    var lebarError by remember { mutableStateOf(false) }
+    var lebar by rememberSaveable { mutableStateOf("") }
+    var lebarError by rememberSaveable { mutableStateOf(false) }
 
-    var luas by remember { mutableFloatStateOf(0f) }
-    var keliling by remember { mutableFloatStateOf(0f) }
+    var luas by rememberSaveable { mutableFloatStateOf(0f) }
+    var keliling by rememberSaveable { mutableFloatStateOf(0f) }
 
     val symbols = DecimalFormatSymbols(Locale("id", "ID")).apply {
         decimalSeparator = ','
@@ -79,6 +98,8 @@ fun ScreenContent(modifier: Modifier) {
     val formatter = DecimalFormat("#,##0.00", symbols)
     val formattedArea = formatter.format(luas)
     val formattedPerimeter = formatter.format(keliling)
+
+    val context = LocalContext.current
 
     Column(
         modifier = modifier
@@ -98,7 +119,7 @@ fun ScreenContent(modifier: Modifier) {
             onValueChange = {panjang = it },
             label = { Text(text = stringResource(id = R.string.panjang))},
             isError = panjangError,
-            trailingIcon = { IconPicker(panjangError, unit = "kg" )},
+            trailingIcon = { IconPicker(panjangError, unit = "cm" )},
             supportingText = { ErrorHint(panjangError) },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
@@ -163,6 +184,16 @@ fun ScreenContent(modifier: Modifier) {
         ) {
             Text(text = stringResource(id = R.string.reset))
         }
+        Button(
+            onClick = { shareData(
+                context = context,
+                message = context.getString(R.string.bagikan_template,panjang,lebar,luas,keliling)
+            ) },
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+           Text(text = stringResource(id = R.string.bagikan))
+        }
     }
 }
 
@@ -190,11 +221,21 @@ fun ErrorHint(isError: Boolean) {
     }
 }
 
+private fun shareData(context: Context, message: String) {
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    if (shareIntent.resolveActivity(context.packageManager) !=null) {
+        context.startActivity(shareIntent)
+    }
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun GreetingPreview() {
     Mobpro1Theme {
-        MainScreen()
+        MainScreen(rememberNavController())
     }
 }
