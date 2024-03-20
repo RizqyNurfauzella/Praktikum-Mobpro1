e22ea87cd2b7e6f25febdc50a16817d7e67b69c0
RizqyNurfauzella <fauzella171@gmail.com>
2024-03-20 13:09:26 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
@@ -16,10 +16,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -43,13 +45,16 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3074.mobpro1.R
+import org.d3if3074.mobpro1.navigation.Screen
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 import kotlin.math.pow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavHostController) {
     Scaffold (
       topBar = {
           TopAppBar(
@@ -59,7 +64,18 @@ fun MainScreen() {
               colors = TopAppBarDefaults.mediumTopAppBarColors(
                   containerColor = MaterialTheme.colorScheme.primaryContainer,
                   titleContentColor = MaterialTheme.colorScheme.primary
-              )
+              ),
+              actions = {
+                  IconButton(onClick = {
+                      navController.navigate(Screen.About.route)
+                  }) {
+                      Icon(
+                          imageVector = Icons.Outlined.Info,
+                          contentDescription = stringResource(id = R.string.tentang_aplikasi),
+                          tint = MaterialTheme.colorScheme.primary
+                      )
+                  }
+              }
           )
         }
     ) { padding ->
@@ -232,6 +248,6 @@ private fun getKategori(bmi: Float, isMale: Boolean): Int {
 @Composable
 fun GreetingPreview() {
     Mobpro1Theme {
-        MainScreen()
+        MainScreen(rememberNavController())
     }
 }
