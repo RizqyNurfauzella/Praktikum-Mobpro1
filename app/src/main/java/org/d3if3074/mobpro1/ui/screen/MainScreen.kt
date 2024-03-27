1b966a8a2a298a03f196e65a6a11d386b944e8fc
RizqyNurfauzella <fauzella171@gmail.com>
2024-03-27 12:48:05 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -52,7 +53,17 @@ fun ScreenContent(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
 
-    LazyColumn(
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    }
+    else {
+        LazyColumn(
             modifier = modifier.fillMaxSize()
         ) {
             items(data) {
@@ -61,6 +72,7 @@ fun ScreenContent(modifier: Modifier) {
             }
         }
     }
+}
 
 @Composable
 fun ListItem(catatan: Catatan) {
