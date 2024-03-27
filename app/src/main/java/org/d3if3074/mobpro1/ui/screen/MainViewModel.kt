24a15a40cb3e845ed64685d0b4fc3c99c3a8715f
RizqyNurfauzella <fauzella171@gmail.com>
2024-03-27 12:40:51 +0700

    Membuat tampilan List

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
@@ -5,17 +5,17 @@ import org.d3if3074.mobpro1.model.Catatan
 
 class MainViewModel : ViewModel() {
 
-    val data = getDataDumbby()
+    val data = getDataDummy()
 
-    private fun getDataDumbby(): List<Catatan> {
+    private fun getDataDummy(): List<Catatan> {
         val data = mutableListOf<Catatan>()
         for (i in 29 downTo 20) {
             data.add(
                 Catatan(
                     i.toLong(),
-                    "Kuloah Mobpro $1 Maret",
+                    "Kuliah Mobpro $i Maret",
                     "Yey, hari ini belajar membuat aplikasi Android List dan berhasil, Hehe.. Mudah2an modul selanjutnya juga lancar. Aamiin.",
-                    "2024-03-$1 12:11:50"
+                    "2024-03-$i 12:11:50"
                 )
             )
         }
