12e67c547b0983dbf8074c344cf015f1dcdc25e0
RizqyNurfauzella <fauzella171@gmail.com>
2024-04-19 00:19:38 +0700

    Cherrypick from Catatan

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/DetailScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/DetailScreen.kt
@@ -11,7 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -34,6 +39,8 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3074.mobpro1.R
 import org.d3if3074.mobpro1.ui.theme.Mobpro1Theme
 
@@ -55,6 +62,15 @@ fun DetailScreen() {
     Scaffold(
         topBar   = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = {
                     Text(text = stringResource(id = R.string.tambah_mahasiswa))
                 },
@@ -62,6 +78,15 @@ fun DetailScreen() {
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
+                actions = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = stringResource(R.string.simpan),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -151,6 +176,6 @@ fun KelasOption(label: String, isSelected: Boolean, modifier: Modifier) {
 @Composable
 fun DetailScreenPreview() {
     Mobpro1Theme {
-        DetailScreen()
+        DetailScreen(rememberNavController())
     }
 }
