8f383f97aa69144510e82c1719a5e1cfc30e21c7
RizqyNurfauzella <fauzella171@gmail.com>
2024-04-24 13:11:50 +0700

    Mengambil data dari database

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
@@ -1,24 +1,18 @@
 package org.d3if3074.mobpro1.ui.screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import org.d3if3074.mobpro1.database.CatatanDao
 import org.d3if3074.mobpro1.model.Catatan
 
-class MainViewModel : ViewModel() {
+class MainViewModel(dao: CatatanDao) : ViewModel() {
 
-    val data = getDataDummy()
-
-    private fun getDataDummy(): List<Catatan> {
-        val data = mutableListOf<Catatan>()
-        for (i in 29 downTo 20) {
-            data.add(
-                Catatan(
-                    i.toLong(),
-                    "Kuliah Mobpro $i Maret",
-                    "Yey, hari ini belajar membuat aplikasi Android List dan berhasil, Hehe.. Mudah2an modul selanjutnya juga lancar. Aamiin.",
-                    "2024-03-$i 12:11:50"
-                )
-            )
-        }
-        return data
-    }
+    val data: StateFlow<List<Catatan>> = dao.getCatatan().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = emptyList()
+    )
 }
