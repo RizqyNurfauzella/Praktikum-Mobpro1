a0aa8390dff6794df59b91cc90d8457cbb8cecf8
RizqyNurfauzella <fauzella171@gmail.com>
2024-05-23 03:14:28 +0700

    Membuat indikator loading JSON

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.d3if3074.mobpro1.model.Hewan
+import org.d3if3074.mobpro1.network.ApiStatus
 import org.d3if3074.mobpro1.network.HewanApi
 
 class MainViewModel : ViewModel() {
@@ -14,14 +16,20 @@ class MainViewModel : ViewModel() {
     var data = mutableStateOf(emptyList<Hewan>())
         private set
 
+    var status = MutableStateFlow(ApiStatus.LOADING)
+        private set
+
+
     init {
         retrieveData()
     }
 
     private fun retrieveData() {
         viewModelScope.launch(Dispatchers.IO) {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = HewanApi.service.getHewan()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
