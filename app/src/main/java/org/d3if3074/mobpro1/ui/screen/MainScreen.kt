92d1405c554a36700eaf5aa321af73939d6711f3
RizqyNurfauzella <fauzella171@gmail.com>
2024-05-29 14:33:51 +0700

    Membuat fitur logout

--- app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3074/mobpro1/ui/screen/MainScreen.kt
@@ -43,10 +43,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.GetCredentialResponse
+import androidx.credentials.exceptions.ClearCredentialException
 import androidx.credentials.exceptions.GetCredentialException
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
@@ -111,6 +113,7 @@ fun MainScreen() {
             ProfilDialog(
                 user = user,
                 onDismissRequest = { showDialog = false }) {
+                CoroutineScope(Dispatchers.IO).launch { signOut(context, dataStore) }
                 showDialog = false
             }
         }
@@ -240,6 +243,19 @@ private suspend fun handleSignIn(result: GetCredentialResponse, dataStore: UserD
         Log.e("SIGN-IN", "Error: unrecognized custom credential type.")
     }
 }
+
+private suspend fun signOut(context: Context, dataStore: UserDataStore) {
+    try {
+        val credentialManager = CredentialManager.create(context)
+        credentialManager.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        dataStore.saveData(User())
+    } catch (e: ClearCredentialException) {
+        Log.e("SIGN-IN", "Error: ${e.errorMessage}")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
